ee8cb630fa48f69f5b3646ec2e133e5d2f1076d4
LypkaB <lpk.bdn@gmail.com>
2021-12-14 20:00:00 +0200

    Add footer

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+'use strict';
+
+window.addEventListener('DOMContentLoaded', () => {
+    const footerMailInput = document.querySelector('.footer__mail-input #footerMail');
+    const footerMailLabel = document.querySelector('.footer__mail-input label[for="footerMail"]');
+
+    footerMailInput.addEventListener('blur', function() {
+        if (this.value.length === 0) footerMailLabel.classList.remove('label_up');
+    })
+
+    footerMailInput.addEventListener('focus', function() {
+        footerMailLabel.classList.add('label_up');
+    })
+})
