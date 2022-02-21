e45d9850a5b44f63a9c41f86c22bf8e30dc3b40a
LypkaB <lpk.bdn@gmail.com>
2022-02-21 18:00:00 +0200

    Add dropdown list on hover header lines

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,11 +4,11 @@ window.addEventListener('DOMContentLoaded', () => {
     const footerMailInput = document.querySelector('.footer__mail-input #footerMail');
     const footerMailLabel = document.querySelector('.footer__mail-input label[for="footerMail"]');
 
-    footerMailInput.addEventListener('blur', function() {
+    footerMailInput.addEventListener('blur', function () {
         if (this.value.length === 0) footerMailLabel.classList.remove('label_up');
     })
 
-    footerMailInput.addEventListener('focus', function() {
+    footerMailInput.addEventListener('focus', function () {
         footerMailLabel.classList.add('label_up');
     })
 })
